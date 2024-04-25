99b5e480297085b44f229dcf5f5ebe2e388323c1
Hari Turlapati <turlapati@gmail.com>
2024-04-25 19:21:07 -0400

    Usability improvements

--- cwmp_parameter_lookup.py
+++ cwmp_parameter_lookup.py
@@ -1,5 +1,6 @@
 import xml.etree.ElementTree as ET
-from tkinter import BooleanVar, Checkbutton, Entry, Frame, Label, Listbox, Menu, Scrollbar, Tk
+from tkinter import BooleanVar, Button, Checkbutton, Entry, Frame, Label, Listbox, Menu, Scrollbar, Tk
+from tkinter import LEFT, RIGHT, MULTIPLE
 from tkinter import filedialog, messagebox
 from typing import List
 
@@ -34,6 +35,30 @@ def read_tr181_file(file_name: str) -> List[str]:
         return []
 
 
+def update_listbox(parameters: List[str], case_ref: BooleanVar, entry_ref: Entry, listbox_ref: Listbox) -> None:
+    # Clear the listbox
+    listbox_ref.delete(0, 'end')
+
+    if case_ref.get():
+        partial_string: str = entry_ref.get()
+        for item in parameters:
+            if partial_string in item:
+                listbox_ref.insert('end', item)
+    else:
+        partial_string: str = entry_ref.get().lower()
+        for item in parameters:
+            if partial_string in item.lower():
+                listbox_ref.insert('end', item)
+
+
+def copy_to_clipboard(listbox_ref: Listbox) -> None:
+    root.clipboard_clear()
+    selected = listbox_ref.curselection()
+    if selected:
+        for i in selected:
+            root.clipboard_append(listbox_ref.get(i) + '\n')
+
+
 def click_on_file_open() -> None:
     """Select a TR-181 Data Model File"""
     file_name: str = filedialog.askopenfilename(filetypes=[('XML Files', '*.xml')])
@@ -50,24 +75,8 @@ def click_on_file_open() -> None:
             for parameter in parameters:
                 listbox.insert('end', parameter)
 
-            # Function to update the listbox
-            def update_listbox(event) -> None:
-                # Clear the listbox
-                listbox.delete(0, 'end')
-
-                if case_sensitive.get():
-                    partial_string: str = entry.get()
-                    for item in parameters:
-                        if partial_string in item:
-                            listbox.insert('end', item)
-                else:
-                    partial_string: str = entry.get().lower()
-                    for item in parameters:
-                        if partial_string in item.lower():
-                            listbox.insert('end', item)
-
             # Bind the function to the entry widget
-            entry.bind('<KeyRelease>', update_listbox)
+            entry.bind('<KeyRelease>', lambda event: update_listbox(parameters, case_sensitive, entry, listbox))
 
         except FileNotFoundError:
             # Display an error message if the file is not found
@@ -104,24 +113,39 @@ if __name__ == '__main__':
     file_label.pack(fill='x')
     param_label.pack(fill='x')
 
-    # Create entry and listbox widgets (initially disabled)
-    entry: Entry = Entry(root, state='disabled')
-    entry.pack(fill='x')
+    # Create entry widgets with clear button
+    entry_frame: Frame = Frame(root)
+    entry_frame.pack(anchor='w', fill='x')
+    entry: Entry = Entry(entry_frame, state='disabled')
+    entry.pack(anchor='w', side=LEFT, fill='both', expand=1)
+    # Button to clear selection and restart
+    entry_clear = Button(entry_frame, text="x",
+                         command=lambda: [entry.delete(0, 'end'), entry.event_generate('<KeyRelease>')])
+    entry_clear.pack(side=RIGHT)
 
+    # Create listbox  (initially disabled)
     listbox_frame: Frame = Frame(root)
-    listbox_frame.pack(fill='both', expand=1)
+    listbox_frame.pack(anchor='w', fill='both', expand=1)
     scrollbar: Scrollbar = Scrollbar(listbox_frame)
     scrollbar.pack(side='right', fill='y')
-    listbox: Listbox = Listbox(listbox_frame, yscrollcommand=scrollbar.set, state='disabled')
+    listbox: Listbox = Listbox(listbox_frame, yscrollcommand=scrollbar.set, state='disabled', selectmode=MULTIPLE)
     listbox.pack(fill='both', expand=1)
     scrollbar.config(command=listbox.yview)
+
+    # Define a clippy!!
+    listbox.bind('<Button-3>', lambda e: popup_menu.tk_popup(e.x_root, e.y_root))
+    popup_menu = Menu(root, tearoff=0)
+    popup_menu.add_command(label='Copy', command=lambda: copy_to_clipboard(listbox))
 
     # Variable to store case-sensitive search state
     case_sensitive: BooleanVar = BooleanVar(value=False)  # Initially case-insensitive
 
     # Create checkbox for case-sensitive search
     case_sensitive_checkbox: Checkbutton = Checkbutton(root, text="Case-Sensitive Search", variable=case_sensitive)
-    case_sensitive_checkbox.pack(anchor='w')
+    case_sensitive_checkbox.pack(anchor='w', side=LEFT)
+    # Button to clear selection and restart
+    listbox_clear = Button(root, text="Clear selection", command=lambda: listbox.selection_clear(0, 'end'))
+    listbox_clear.pack(side=RIGHT)
 
     # Start the GUI application
     root.mainloop()
